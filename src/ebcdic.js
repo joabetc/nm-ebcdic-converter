4cca4a79f8ad0949094307382e38f07a48a58204
Joabe Costa <joabetc@hotmail.com>
2018-08-11 10:14:02 -0300

    Passing test for function fromASCII

--- src/ebcdic.js
+++ src/ebcdic.js
@@ -4,5 +4,13 @@ map.set('C6', 46);
 module.exports = {
   toASCII: function(char) {
     return String.fromCharCode(parseInt(map.get(char), 16));
+  },
+  fromASCII: function(char) {
+    let ret;
+    map.forEach((value, key, map) => {
+      if (String.fromCharCode(parseInt(value, 16)) === char)
+        ret = key;
+    });
+    return ret;
   }
 }
