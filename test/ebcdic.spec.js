01bc025a115aaebb459fd971eedd136a336b55a0
Joabe Costa <joabetc@hotmail.com>
2018-08-10 22:37:58 -0300

    Formating code

--- test/ebcdic.spec.js
+++ test/ebcdic.spec.js
@@ -4,8 +4,11 @@ var assert = require('assert');
 describe('ebcdic', function() {
 
   context('when converting to ASCII', function() {
+    
     it('should return F for C6', function() {
       assert.equal(ebcdic.toASCII('C6'), 'F');
-    })
-  })
+    });
+
+  });
+  
 });
