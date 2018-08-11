075032c2c89bff151bfad33dcce14e8772d2299f
Joabe Costa <joabetc@hotmail.com>
2018-08-11 11:12:55 -0300

    Improving code ES6

--- src/ebcdic.js
+++ src/ebcdic.js
@@ -1,19 +1,21 @@
 const map = new Map();
 map.set('C6', 46);
 
-module.exports = {
-  toASCII: function(char) {
-    return String.fromCharCode(parseInt(map.get(char), 16));
-  },
-  fromASCII: function(char) {
+class EBCDIC {
+  toASCII(char) {
+    return this.getCharFromHex(map.get(char));
+  }
+  fromASCII(char) {
     let ret;
     map.forEach((value, key, map) => {
-      if (String.fromCharCode(parseInt(value, 16)) === char)
-        ret = key;
+      if (this.getCharFromHex(value) === char)
+      ret = key;
     });
     return ret;
-  },
-  getCharFromHex: function(hex) {
+  }
+  getCharFromHex(hex) {
     return String.fromCharCode(parseInt(hex, 16));
   }
 }
+
+module.exports = new EBCDIC();
