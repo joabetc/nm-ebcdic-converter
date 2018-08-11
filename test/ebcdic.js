772eb5c8013e6c4ea30a0881ba8c7643a34db8b9
Joabe Costa <joabetc@hotmail.com>
2018-08-10 22:20:30 -0300

    Creating first test

---
+++ test/ebcdic.js
@@ -0,0 +1,11 @@
+var ebcdic = require('../src/ebcdic.js')
+var assert = require('assert');
+
+describe('ebcdic', function() {
+
+  context('when converting to ASCII', function() {
+    it('should return F for C6', function() {
+      assert.equal(ebcdic.toASCII('C6'), 'F');
+    })
+  })
+});
