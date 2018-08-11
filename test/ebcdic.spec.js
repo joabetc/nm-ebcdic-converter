0be01538e0e7c5ccb14acfacbad5e386c43e01f2
Joabe Costa <joabetc@hotmail.com>
2018-08-11 00:32:15 -0300

    Creating a new failing test

--- test/ebcdic.spec.js
+++ test/ebcdic.spec.js
@@ -4,11 +4,14 @@ var assert = require('assert');
 describe('ebcdic', function() {
 
   context('when converting to ASCII', function() {
-    
     it('should return F for C6', function() {
       assert.equal(ebcdic.toASCII('C6'), 'F');
     });
-
   });
-  
+
+  context('when converting from ASCII', function() {
+    it('should return C6 for F', function() {
+      assert.equal(ebcdic.fromASCII('F'), 'C6');
+    });
+  });
 });
